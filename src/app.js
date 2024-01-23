38fa9bfdab78ead040c11ed97b42f613b8e527d9
Harikrishnasinh Zala <154065571+codeatnowofficial@users.noreply.github.com>
2024-01-23 09:30:55 +0530

    added the user and controller with cloudinary utils

--- src/app.js
+++ src/app.js
@@ -1,12 +1,14 @@
 import express from "express";
 import cors from "cors"
 import cookieParser from "cookie-parser"
+import {router} from "./routes/user.routes.js";
 
 const app = express()
 app.use(cors({
     origin:process.env.CORS_ORIGIN,
     credentials:true
 }))
+app.use("/api/v1/users",router)
 app.use(express.json({limit:'16kb'}))
 app.use(express.urlencoded({extended:true,limit:'16kb'}))
 app.use(cookieParser())
--- src/utils/asyncHandler.js
+++
@@ -1,6 +0,0 @@
-export const asyncHandler = (requestHandler) =>{
-    (req,res,next) =>{
-        Promise.resolve(requestHandler(request,response,next))
-        .catch(error => next(error)) 
-    }
-}
